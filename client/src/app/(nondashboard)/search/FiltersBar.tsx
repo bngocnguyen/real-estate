06c78f45a296cbaf24cc0986a6e2d5cc3361bfe5
Ngoc Nguyen <bngocnguyen94@gmail.com>
2026-02-02 22:33:46 +0700

    feat: cont

---
+++ client/src/app/(nondashboard)/search/FiltersBar.tsx
@@ -0,0 +1,154 @@
+import { useAppSelector } from "@/state/redux";
+import { usePathname, useRouter } from "next/navigation";
+import React, { use, useState } from "react";
+import { useDispatch } from "react-redux";
+import { FiltersState, setFilters, toggleFiltersFullOpen } from "@/state";
+import { debounce } from "lodash";
+import { cleanParams, cn, formatPriceValue } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Filter, Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const FiltersBar = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const pathname = usePathname();
+  const filters = useAppSelector((state) => state.global.filters);
+  const isFiltersFullOpen = useAppSelector(
+    (state) => state.global.isFiltersFullOpen,
+  );
+
+  const viewMode = useAppSelector((state) => state.global.viewMode);
+  const [searchInput, setSearchInput] = useState(filters.location);
+
+  const updateURL = debounce((newFilters: FiltersState) => {
+    const cleanFilters = cleanParams(newFilters);
+    const updatedSearchParams = new URLSearchParams();
+
+    Object.entries(cleanFilters).forEach(([key, value]) => {
+      updatedSearchParams.set(
+        key,
+        Array.isArray(value) ? value.join(",") : value.toString(),
+      );
+    });
+
+    router.push(`${pathname}?${updatedSearchParams.toString()}`);
+  });
+
+  const handleFilterChange = (
+    key: string,
+    value: any,
+    isMin: boolean | null,
+  ) => {
+    let newValue = value;
+
+    if (key === "priceRange" || key === "squareFeet") {
+      const currentArrayRange = [...filters[key]];
+      if (isMin != null) {
+        const index = isMin ? 0 : 1;
+        currentArrayRange[index] = value === "any" ? null : Number(value);
+      }
+      newValue = currentArrayRange;
+    } else if (key === "coordinates") {
+      newValue = value === "any" ? [0, 0] : value.map(Number);
+    } else {
+      newValue = value === "any" ? "any" : value;
+    }
+
+    const newFilters = { ...filters, [key]: newValue };
+    dispatch(setFilters(newFilters));
+    updateURL(newFilters);
+  };
+
+  return (
+    <div className="flex justify-between items-center w-full py-5">
+      {/* <Filters/> */}
+      <div className="flex justify-between items-center gap-4 p-2">
+        <Button
+          variant="outline"
+          className={cn(
+            "gap-2 rounded-xl border-primary-400 hover:bg-primary-500 hover:text-primary-100",
+            isFiltersFullOpen && "bg-primary-700 text-primary-100",
+          )}
+          onClick={() => dispatch(toggleFiltersFullOpen())}
+        >
+          <Filter className="w-4 h-4" />
+          <span>All Filters</span>
+        </Button>
+
+        {/* Search Location */}
+        <div className="flex items-center">
+          <Input
+            placeholder="Search location"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="w-40 rounded-l-xl rounded-r-none border-primary-400 border-r-0"
+          />
+          <Button
+            // onClick={handleLocationSearch}
+            className={`rounded-r-xl rounded-l-none border-l-none border-primary-400 shadow-none 
+              border hover:bg-primary-700 hover:text-primary-50`}
+          >
+            <Search className="w-4 h-4" />
+          </Button>
+        </div>
+
+        {/* Price Range */}
+        <div className="flex gap-1">
+          {/* Minimum Price Selector */}
+          <Select
+            value={filters.priceRange[0]?.toString() || "any"}
+            onValueChange={(value) =>
+              handleFilterChange("priceRange", value, true)
+            }
+          >
+            <SelectTrigger className="w-22 rounded-xl border-primary-400">
+              <SelectValue>
+                {formatPriceValue(filters.priceRange[0], true)}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent className="bg-white">
+              <SelectItem value="any">Any Min Price</SelectItem>
+              {[500, 1000, 1500, 2000, 3000, 5000, 10000].map((price) => (
+                <SelectItem key={price} value={price.toString()}>
+                  ${price / 1000}k+
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          {/* Maximum Price Selector */}
+          <Select
+            value={filters.priceRange[1]?.toString() || "any"}
+            onValueChange={(value) =>
+              handleFilterChange("priceRange", value, false)
+            }
+          >
+            <SelectTrigger className="w-22 rounded-xl border-primary-400">
+              <SelectValue>
+                {formatPriceValue(filters.priceRange[1], false)}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent className="bg-white">
+              <SelectItem value="any">Any Max Price</SelectItem>
+              {[1000, 2000, 3000, 5000, 10000].map((price) => (
+                <SelectItem key={price} value={price.toString()}>
+                  &lt;${price / 1000}k
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FiltersBar;
